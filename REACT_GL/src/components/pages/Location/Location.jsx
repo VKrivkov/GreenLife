156d501d729cd2263627eaf9710cc5c3f1c5069c
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2024-03-16 18:05:16 +0100

    Map Icon added

--- REACT_GL/src/components/pages/Location/Location.jsx
+++ REACT_GL/src/components/pages/Location/Location.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './Location.css';
+import smallLogo from '../../../assets/Logo.png';
 
 const Location = () => {
   const position = [34.832139, 33.584444]; // Your coordinates
-  
+
+  const customIcon = new Icon ({
+    iconUrl: smallLogo,
+    iconSize: [30,30]
+  });
+
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={true} className="map-container">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      <Marker position={position} icon={customIcon}>
         <Popup>
           GreenLife.
         </Popup>
