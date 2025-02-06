5627d00aeda8b80b0a7351cd852277413c63fb99
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2025-02-06 19:24:39 +0100

    Maps Update 1

--- REACT_GL/src/components/pages/Location/Location.jsx
+++ REACT_GL/src/components/pages/Location/Location.jsx
@@ -29,27 +29,29 @@ const Location = () => {
   });
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true} className="map-container">
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position} icon={customIcon}>
-        <Popup>
-          Park Residences
-        </Popup>
-      </Marker>
-      <Marker position={supermarket_loc} icon={shopIcon}>
-        <Popup>
-          Supermarket
-        </Popup>
-      </Marker>
-      <Marker position={school_loc} icon={schoolIcon}>
-        <Popup>
-          School and kindergarten
-        </Popup>
-      </Marker>
-    </MapContainer>
+    // <MapContainer center={position} zoom={13} scrollWheelZoom={true} className="map-container">
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   />
+    //   <Marker position={position} icon={customIcon}>
+    //     <Popup>
+    //       Park Residences
+    //     </Popup>
+    //   </Marker>
+    //   <Marker position={supermarket_loc} icon={shopIcon}>
+    //     <Popup>
+    //       Supermarket
+    //     </Popup>
+    //   </Marker>
+    //   <Marker position={school_loc} icon={schoolIcon}>
+    //     <Popup>
+    //       School and kindergarten
+    //     </Popup>
+    //   </Marker>
+    // </MapContainer>
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d6383.702323957627!2d33.58031143664141!3d34.83252959484982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzTCsDQ5JzU1LjkiTiAzM8KwMzUnMDQuMCJF!5e1!3m2!1sen!2slu!4v1738865102393!5m2!1sen!2slu" width={"100%"} height={"100%"} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   );
 };
 
